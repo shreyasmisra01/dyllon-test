8616723deb5916b7d5e55f493d3e4bf45aa33ecb
shreyasmisra01 <shreyas.misra@laitkor.com>
2025-04-16 12:32:06 +0530

    updated code for the issues in landmark one, landmark unique, landmark no duplicate banner, aria text and scope attr valid

--- app/components/AriaDeprecatedRole.tsx
+++ app/components/AriaDeprecatedRole.tsx
@@ -15,9 +15,34 @@ const AriaDeprecatedRole = () => {
       <h1 id="links-title">Elements with deprecated role</h1>
 
       {/* Div having deprecated role*/}
-      <div role="input">
+      {/* <div role="input">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" />
+    </div> */}
+
+<div>
+      {/* Issue 1: Using the deprecated "application" role on a div */}
+      <div role="application">
+        <p>This div is using the deprecated application role.</p>
+      </div>
+
+      {/* Issue 2: Using the deprecated "directory" role on a ul */}
+      <ul role="directory">
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+
+      {/* Issue 3: Using the deprecated "toolbar" role on a div (should be used with aria-orientation) */}
+      <div role="toolbar">
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </div>
+
+      {/* Issue 4: Using the deprecated "menuitemcheckbox" role on a div (should be input type="checkbox" with role="menuitemcheckbox") */}
+      <div role="menuitemcheckbox" aria-checked="false">Checkbox Item</div>
+
+      {/* Issue 5: Using the deprecated "menuitemradio" role on a div (should be input type="radio" with role="menuitemradio") */}
+      <div role="menuitemradio" aria-checked="false">Radio Item</div>
     </div>
     </>
   );
--- app/components/ScopeAtrrValid.tsx
+++ app/components/ScopeAtrrValid.tsx
@@ -15,7 +15,38 @@ const ScopeAttrValid = () => {
       <h1 id="links-title">Elements with invalid scope property</h1>
 
       {/* Div having invalid scope */} 
-      <div scope="row">Invalid Div having scope=row</div>
+      {/* <div scope="row">Invalid Div having scope=row</div> */}
+
+      <table>
+      <caption>Product Comparison</caption>
+      <thead>
+        <tr>
+          <th>Feature</th>
+          <th>Product A</th>
+          <th>Product B</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {/* Incorrect scope attribute - should be "row" for a data cell in the first column */}
+          <th scope="col">Price</th>
+          <td>$19.99</td>
+          <td>$24.99</td>
+        </tr>
+        <tr>
+          {/* Missing scope attribute - should be "row" for a data cell in the first column */}
+          <th>Availability</th>
+          <td>In Stock</td>
+          <td>Out of Stock</td>
+        </tr>
+        <tr>
+          {/* Incorrect scope attribute - should be "col" for a header cell in the header row */}
+          <th scope="row">Rating</th>
+          <td>4.5 Stars</td>
+          <td>4.2 Stars</td>
+        </tr>
+      </tbody>
+    </table>
     </>
   );
 };
